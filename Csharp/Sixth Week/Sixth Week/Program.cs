443ef3baf74c2b56aac9d391942fc05fa61a7e86
Scientific Association of semnan university's Computer Science <cs.sem.scientific@gmail.com>
2022-09-09 20:29:08 +0430

    Add files via upload

---
+++ Csharp/Sixth Week/Sixth Week/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+/**
+ * @file ProblemTwo.cs
+ * @author Amir khaksar (ah_khaksar@yahoo.com)
+ * @brief count letters
+ * @version 0.1.0
+ * @date 2022-09-09
+ * @id 2code2
+ * @Compailer dot net
+ * @copyright Copyright (c) 2022
+ *
+ */
+namespace Sixth_Week
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int V = Convert.ToInt16(Console.ReadLine());
+            int E = Convert.ToInt16(Console.ReadLine());
+
+            for (int i = 0; i < E; i++)
+            {
+                int src = Convert.ToInt16(Console.ReadLine());
+                int dest = Convert.ToInt16(Console.ReadLine());
+            }
+            Console.WriteLine((V < E ? 0 : 1));
+        }
+    }
+}
